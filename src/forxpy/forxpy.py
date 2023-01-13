2b2e56fc43fec63c2022f34ba65493ec99970f0d
Hongjian Li <samli016@student.ubc.ca>
2023-01-12 17:56:35 -0800

    add docstirng for the conversion

---
+++ src/forxpy/forxpy.py
@@ -0,0 +1,25 @@
+def convert(Currency1,Currency2):
+    """Convert Currency 1 to Currency 2.
+
+    The conversion rate is based on the average exchange rate 
+    by the 4:00 pm ET of the closest business day
+
+
+    Parameters
+    ----------
+    Currency1: str
+        The type of currency to be converted
+    
+    Currency2: str
+        The type of currency that the currency1 will be converted to
+
+    Returns
+    -------
+    float
+        The amount of currency 2 that the currency 1 equals to 
+
+    Examples
+    --------
+    >>> convert("CAD","USD")
+    """
+    return ...
