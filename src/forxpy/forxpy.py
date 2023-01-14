e876e6743476da8f61869d2684bc47b5c0003aac
dhruvinishar <dhruvi.bs19bds030@spjain.org>
2023-01-13 16:22:14 -0800

    updates

--- src/forxpy/forxpy.py
+++ src/forxpy/forxpy.py
@@ -1,16 +1,16 @@
-def convert(Currency1,Currency2):
-    """Convert Currency 1 to Currency 2.
+def convert(currency1,currency2):
+    """
+    Convert Currency 1 to Currency 2.
 
     The conversion rate is based on the average exchange rate 
     by the 4:00 pm ET of the closest business day
 
-
     Parameters
     ----------
-    Currency1: str
+    currency1: str
         The type of currency to be converted
     
-    Currency2: str
+    currency2: str
         The type of currency that the currency1 will be converted to
 
     Returns
@@ -22,12 +22,13 @@ def convert(Currency1,Currency2):
     --------
     >>> convert("CAD","USD")
     """
-    return ...
+    pass
 
-def retrieve_data("YYYY-mm-dd", "/output_file.csv"):
+def retrieve_data(start_date, output_path):
 	    
 	"""
-	Retrieve historical daily currency exchange rate data for Canadian Dollars in CSV format from the Bank of Canada website.    
+	Retrieve historical daily currency exchange rate data for Canadian Dollars 
+    in CSV format from the Bank of Canada website.    
 	
 	Parameters
 	----------
@@ -35,8 +36,7 @@ def retrieve_data("YYYY-mm-dd", "/output_file.csv"):
 	    inputted starting date in the format specified "%YYYY-%mm-%dd"
 	output_path : path
 	    the path where the csv file will get written to
-		
-	    
+		 
 	Returns
 	-------
 	file
@@ -45,26 +45,58 @@ def retrieve_data("YYYY-mm-dd", "/output_file.csv"):
 	Examples
 	>>> retrieve_data("2022-05-23", "my_document/currency_exchange.csv")
 	"""
-	
+	pass
     
-def fastest_slowest_currency(dataframe):
+def fastest_slowest_currency(df):
     
     """
-    This function takes currency exchange rates data as input and returns a list of two strings containing the fastest and slowest growing currency exchange rate in relation to Canadian Dollar.
-    The data provided contains currency code in the format FX***CAD, the average exchange rate and the date.
+    This function takes currency exchange rates data as input and returns a 
+    list of two strings containing the fastest and slowest growing currency 
+    exchange rate in relation to Canadian Dollar.
+    The data provided contains currency code in the format FX***CAD, 
+    the average exchange rate and the date.
     
     Parameters
     ----------
-    data_frame : Pandas DataFrame
+    df : Pandas DataFrame
         data frame containing currency exchange rates to Canadian Dollars by date
     
     Returns
     -------
     list
-        list of strings containing the currency name used to convert to CAD in the format (FX***CAD) and the exchange rates for the fastest and slowest growing currencies
+        list of strings containing the currency name used to convert 
+        to CAD in the format (FX***CAD) and the exchange rates for 
+        the fastest and slowest growing currencies
     
     Examples
     >>> fastest_slowest_currency(exchange_data)
     ['FXUSDCAD: 0.857', 'FXTHBCAD: 0.263']
+    """ 
+    pass
+
+def currency_convert(value, currency1, currency2):
     """
+    This function takes a currency value and the currency type 
+    to be converted to as input and returns the converted currency 
+    value as per the current conversion rate.
     
+    Parameters
+    ----------
+    value: float
+        The value of the original currency to be converted
+
+    currency1: str
+        The type of currency originally
+    
+    currency2: str
+        The type of currency that the currency1 will be converted to
+    
+    Returns
+    -------
+    converted: numeric
+        Returns converted numeric currency
+    
+    Examples
+    >>> currency_convert(23, 'USD', 'CAD')
+    """
+    pass
