87447960e655cba94bff03f9c8167eb7eb16f403
Mohammad Reza Nabizadeh <mrnabiz@student.ubc.ca>
2023-01-13 18:23:11 -0800

    added readme descriptions

--- src/forxpy/forxpy.py
+++ src/forxpy/forxpy.py
@@ -1,39 +1,12 @@
-def convert(currency1,currency2):
-    """
-    Convert Currency 1 to Currency 2.
-
-    The conversion rate is based on the average exchange rate 
-    by the 4:00 pm ET of the closest business day
-
-    Parameters
-    ----------
-    currency1: str
-        The type of currency to be converted
-    
-    currency2: str
-        The type of currency that the currency1 will be converted to
-
-    Returns
-    -------
-    float
-        The amount of currency 2 that the currency 1 equals to 
-
-    Examples
-    --------
-    >>> convert("CAD","USD")
-    """
-    pass
-
-def retrieve_data(start_date, output_path):
-	    
+def retrieve_data(start_date, output_path):    
 	"""
 	Retrieve historical daily currency exchange rate data for Canadian Dollars 
     in CSV format from the Bank of Canada website.    
 	
 	Parameters
 	----------
-	start_date : string "%YYYY-%mm-%dd"
-	    inputted starting date in the format specified "%YYYY-%mm-%dd"
+	start_date : string '%YYYY-%mm-%dd'
+	    inputted starting date in the format specified '%YYYY-%mm-%dd'
 	output_path : path
 	    the path where the csv file will get written to
 		 
@@ -43,12 +16,11 @@ def retrieve_data(start_date, output_path):
 	    csv data file that includes historical data on currency exchange rates
 	    
 	Examples
-	>>> retrieve_data("2022-05-23", "my_document/currency_exchange.csv")
+	>>> retrieve_data('2022-05-23', 'my_document/currency_exchange.csv')
 	"""
 	pass
     
 def fastest_slowest_currency(df):
-    
     """
     This function takes currency exchange rates data as input and returns a 
     list of two strings containing the fastest and slowest growing currency 
@@ -98,5 +70,30 @@ def currency_convert(value, currency1, currency2):
     
     Examples
     >>> currency_convert(23, 'USD', 'CAD')
+    """
+    pass
+
+def plot_historical(start_date, end_date, currency):
+    """
+    Plots the historical rate of the entered currencies within a specific period
+    of time.
+
+    Parameters
+    ----------
+	start_date : string '%YYYY-%mm-%dd'
+	    inputted starting date in the format specified '%YYYY-%mm-%dd'
+	end_date : string '%YYYY-%mm-%dd'
+	    inputted ending date in the format specified '%YYYY-%mm-%dd'
+    currency : str
+        The type of currency asked for plotting
+
+    Returns
+    -------
+    plot object
+        A plot showing the performance of the currency.
+
+    Examples
+    --------
+    >>> plot_historical('2022-05-23', '2022-05-30', 'USD')
     """
     pass
